40cf251d2518254eab01e34d47e387b969a8afd1
Elvira Rui Xiong <rxiong@umass.edu>
2022-05-15 11:03:18 -0400

    popup branch

--- welcome.js
+++ welcome.js
@@ -1,5 +1,5 @@
 function getData(){
-    let name = document.getElementById("name");
+    let name = document.getElementById("name").textContent;
     console.log(name)
     localStorage.setItem("fname", name)
     document.getElementById("head1").textContent = name;
